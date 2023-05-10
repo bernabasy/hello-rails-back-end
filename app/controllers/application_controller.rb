605a04956b0d0c53bc6be70c437e2d4c9a2e8ffe
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-10 22:44:09 +0300

    Add set_cors_headers method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    before_action :set_cors_headers
+
+    def set_cors_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    end
 end
