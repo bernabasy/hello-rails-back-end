91db75d69d0d882fc4aa407f92147ff7b013ab5b
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-05-10 18:21:39 +0300

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create!(greeting: 'Welcome to the future!')
+Greeting.create!(greeting: 'Greetings, traveler!')
+Greeting.create!(greeting: 'Hello, world changer!')
+Greeting.create!(greeting: 'Hi there, innovator!')
+Greeting.create!(greeting: 'Good day, game changer!')
